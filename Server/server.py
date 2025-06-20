3da04b52e9400397c1a48e4fecd6006e9ac213e3
nam tran trong <nam.tran-trong@ubisoft.com>
2025-06-20 11:04:34 +0700

    update

---
+++ Server/server.py
@@ -0,0 +1,18 @@
+﻿import firebase_admin
+from firebase_admin import credentials, db
+cred = credentials.Certificate("service-account-key.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://remote-controller-356c1-default-rtdb.asia-southeast1.firebasedatabase.app/'
+})
+
+# ref = db.reference('/users')  # đường dẫn gốc
+# ref.set({
+#     'user1': {
+#         'name': 'Alice',
+#         'age': 25
+#     },
+#     'user2': {
+#         'name': 'Bob',
+#         'age': 30
+#     }
+# })
